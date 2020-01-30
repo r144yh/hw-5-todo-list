eec145f3845e46ea74df3a65138c4562bb311f54
r144yh <mrakur@yandex.ru>
2020-01-30 13:56:31 +0300

    Update application architecture

--- toDoList.js
+++ toDoList.js
@@ -1,44 +1,67 @@
 'use strict';
-
-function newItem() {
-    var item = document.getElementById('elementToDo').value;
+var initToDo = (function () {
     var ul = document.getElementById('toDoList');
-    var li = document.createElement('li');
-    var checkButton = document.createElement('INPUT');
-    var removeButton = document.createElement('button');
 
-    checkButton.setAttribute('type', 'checkbox');
-    removeButton.textContent = 'Don\'t do it \u2716';
-    removeButton.classList.add('removeItem');
-
-    if (item.length === 0 || !item.trim()) {
-        return;
+    function eventListener() {
+        document.body.addEventListener('keydown', function (event) {
+            if (event.code === "Enter") {
+                newItem();
+            }
+        });
     }
 
-    li.appendChild(checkButton);
-    li.appendChild(document.createTextNode(item));
-    li.appendChild(removeButton);
-    ul.appendChild(li);
+    function newItem() {
+        var item = document.getElementById('elementToDo').value;
+        var li = document.createElement('li');
+        var textItem = document.createTextNode(item);
+        var checkButton = document.createElement('INPUT');
+        var removeButton = document.createElement('button');
 
+        checkButton.setAttribute('type', 'checkbox');
+        removeButton.textContent = 'Don\'t do it \u2716';
+        removeButton.classList.add('removeItem');
 
-    document.getElementById('elementToDo').value = '';
-
-    li.lastChild.onclick = function () {
-        li.remove();
-    };
-
-    li.firstChild.addEventListener('click', function () {
-        if (checkButton.checked === true) {
-            return li.style.textDecoration = 'line-through';
+        if (item.length === 0 || !item.trim()) {
+            return alert('Add text and then click Enter');
         }
-        li.style.textDecoration = 'none';
-    });
-}
 
-document.body.addEventListener('keydown', function (event) {
-    if (event.code === "Enter") {
-        newItem();
+        li.appendChild(checkButton);
+        li.appendChild(textItem);
+        li.appendChild(removeButton);
+        ul.appendChild(li);
+
+        document.getElementById('elementToDo').value = '';
+
+        li.lastChild.onclick = function () {
+            li.remove();
+        };
+
+        li.firstChild.addEventListener('click', function () {
+            if (checkButton.checked === true) {
+                return li.style.textDecoration = 'line-through';
+            }
+            li.style.textDecoration = 'none';
+        });
     }
-});
+
+    return {
+        init: function () {
+            eventListener();
+        }
+    }
+})();
+
+initToDo.init();
 
 
+// function filterItem() {
+//
+// }
+//
+// function deadlineFilter(){
+//
+// }
+//
+// function checkedItem(){
+//
+// }
